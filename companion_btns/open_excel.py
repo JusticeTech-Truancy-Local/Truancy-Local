e549d61269c2a12b61deb5a936fb9b18cf90e9b3
OThongphayEDU <thongphay.1@osu.edu>
2026-03-10 09:53:52 -0400

    Revert "Created text bars and file directory Persistence"

--- companion_btns/open_excel.py
+++ companion_btns/open_excel.py
@@ -1,20 +1,12 @@
 from PyQt6.QtWidgets import QFileDialog, QMessageBox
 import xlwings as xw
-import os
 
 def open_excel(self):
-    saved_excel_dir = self.settings.value("excel_dir", "/home")
-
-    excel_path = QFileDialog.getOpenFileName(self, "Open Excel File", saved_excel_dir, "Excel Files (*.xlsx *.xls)")[0]
+    excel_path = QFileDialog.getOpenFileName(self, "Open Excel File", "/home", "Excel Files (*.xlsx *.xls)")[0]
     if excel_path:
         try:
             # Open the Excel file with xlwings
             self.workbook = xw.Book(excel_path)
-            self.excel_path_bar.setText(excel_path)
-            self.settings.setValue("excel_path", excel_path)
-            self.settings.setValue("excel_dir", os.path.dirname(excel_path))
-            self.settings.sync()
-
             print(f"Opened Excel file: {excel_path}")
             print(f"Workbook has {len(self.workbook.sheets)} sheet(s)")
             QMessageBox.information(self, "Success", f"Opened Excel file with {len(self.workbook.sheets)} sheet(s)")
